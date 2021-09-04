37e043758d59c560412859c779cd78dbba54eaaa
edsonjabastos <edsonjabastos@hotmail.com>
2021-09-03 22:14:32 -0300

    CSS Modularizado

---
+++ fundamentos/src/pages/estiloso.jsx
@@ -0,0 +1,13 @@
+import styles from '../styles/Estiloso.module.css';
+export default function Extiloso() {
+  return (
+    <div className={styles.roxo}>
+      <h1>Estilo usando CSS Módulos!</h1>
+      <h1>H!</h1>
+      <h2>H@</h2>
+      <h3>H#</h3>
+      <h4>H$</h4>
+      <h5>H%</h5>
+    </div>
+  )
+}
