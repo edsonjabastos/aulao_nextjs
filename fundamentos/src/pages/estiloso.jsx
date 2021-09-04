76748df029e402b7e1eaafd6b5553bb686050ab3
edsonjabastos <edsonjabastos@hotmail.com>
2021-09-04 13:55:34 -0300

    Navegação entre componentes

--- fundamentos/src/pages/estiloso.jsx
+++ fundamentos/src/pages/estiloso.jsx
@@ -1,7 +1,10 @@
 import styles from '../styles/Estiloso.module.css';
+import Link from 'next/link';
+
 export default function Extiloso() {
   return (
     <div className={styles.roxo}>
+      <Link href="/">Voltar!</Link>
       <h1>Estilo usando CSS Módulos!</h1>
       <h1>H!</h1>
       <h2>H@</h2>
--- fundamentos/src/pages/index.jsx
+++ fundamentos/src/pages/index.jsx
@@ -1,8 +1,11 @@
+import Link from 'next/link'
+
 export default function Hello() {
   return (
     <div>
       <h1>Fundamentos Next.js & React!</h1>
       <h2>Vamos estudar esse framework!</h2>
+      <Link href="/estiloso">EXXXTILOSO!</Link>
     </div>
   )
 }
