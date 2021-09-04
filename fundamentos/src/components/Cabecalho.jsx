6714276b45131a0c4ea75c655ecaae63b39d516e
edsonjabastos <edsonjabastos@hotmail.com>
2021-09-04 13:25:43 -0300

    Usando componente

---
+++ fundamentos/src/components/Cabecalho.jsx
@@ -0,0 +1,7 @@
+export default function Cabecalho() {
+  return (
+    <header>
+      <h1>Fundamentos  de Next.JS & React (: :)</h1>
+    </header>
+  )
+}
---
+++ fundamentos/src/pages/exemplo.jsx
@@ -0,0 +1,11 @@
+import Cabecalho from "../components/Cabecalho"
+export default function Exemplo() {
+  return (
+    <>
+    <Cabecalho/>
+    <Cabecalho/>
+    <Cabecalho/>
+    <h1>Exemplo!!!</h1>
+    </>
+  )
+}
