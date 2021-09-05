037a35694181cd1f3e0a6adf1b02564c85649cd6
edsonjabastos <edsonjabastos@hotmail.com>
2021-09-05 12:59:24 -0300

    Pré-rederização

---
+++ fundamentos/src/pages/estatico.jsx
@@ -0,0 +1,16 @@
+import Layout from "../components/Layout"
+
+export function getStaticProps() {
+  return {
+    props: {
+      numero: Math.random()
+    }
+  }
+}
+export default function Estatico(props) {
+  return (
+    <Layout titulo='Conteudo Estático!'>
+      <div>{props.numero}</div>
+    </Layout>
+  )
+}
--- fundamentos/src/pages/index.jsx
+++ fundamentos/src/pages/index.jsx
@@ -17,6 +17,7 @@ export default function Hello() {
       <Navegador texto='Navegação #02' destino='/cliente/mg07/getStoned' cor="#00FFFF"/>
       <Navegador texto='Componente com ESTADO' destino='/estado' cor="#ff7b00"/>
       <Navegador texto='Integração com API #01' destino='/integracao1' cor="#ca2347"/>
+      <Navegador texto='Conteudo Estático' destino='/estatico' cor="#023475"/>
       {/* <h1>Fundamentos Next.js & React!</h1>
       <h2>Vamos estudar esse framework!</h2> */}
     </div>
